6005ef19dae266681b0306094f278fce6e196c3c
edwintineo <edwintineochile@gmail.com>
2025-07-23 23:01:54 -0400

    nuevo2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
-import ThemeProvider from './context/themecontext'
+import ThemeProvider from './context/themecontext.jsx'
 import { CDNProvider, CDNLoadingIndicator } from './components/CDNProvider'
 import PWAInstallPrompt from './components/PWAInstallPrompt'
 
